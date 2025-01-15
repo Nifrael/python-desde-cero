d2e7da3534d36e0d6ab3ee979021924b8f43f336
Julien Marchand <nifrael66@gmail.com>
2025-01-15 19:41:52 +0100

    5e cours

---
+++ 01-basic/05_input.py
@@ -0,0 +1,16 @@
+###
+# 05 - Entrées des utilisateurs - Version simplifiée
+# La fonction input() permet d'obtenir des données de l'utilisateur à travers de la console.
+###
+
+# print("Salut, comment tu t'appelles ?")
+# nom = input()
+
+# print(f"Hola {nom}, enchanté!")
+
+nom = input("Hola, comment tu t'appelles ?\n")
+print(f"Hola {nom}, enchanté!")
+
+age = input("Quel âge as-tu?\n")
+age = int(age)
+print(f"Tu as {age} ans!")
