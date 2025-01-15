17e184bc20295352588d56f676c33303f1b9dd04
Julien Marchand <nifrael66@gmail.com>
2025-01-15 18:51:12 +0100

    Suite

--- 01-basic/01_print.py
+++ 01-basic/01_print.py
@@ -1,1 +1,16 @@
+###
+# 01 -print()
+# Permet d'imprimer tout ce qui se trouve dans les parenthèses dans la console.
+
 print("Hola Mundo")
+print('Fonctionne aussi avec un seul guillemet')
+
+## Imprimer plusieurs valeurs
+
+print("Python", "est", "génial")
+print("Python", "est", "brutal", sep="-")
+
+print("Cela s'imprime", end=" ")
+## Par défaut, la valeur de end est \n et saute une ligne.
+## En changeant le paramètre en chaîne de caractères vide, on peut écrire plusieurs print sur une ligne.
+print("sur une ligne")
