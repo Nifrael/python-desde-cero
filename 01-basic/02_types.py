47f2ef8d1431fe32b5195aede6113e9ad67fad7a
Julien Marchand <nifrael66@gmail.com>
2025-01-15 19:03:44 +0100

    suite

--- 01-basic/02_types.py
+++ 01-basic/02_types.py
@@ -9,3 +9,24 @@ print("int:")
 # print(type(0))
 # print(type(-5))
 # print(type(34787593875923759237590723))
+
+print(34787593875923759237590723 + 1)
+
+print("float:")
+print(type(3.14))
+print(type(1.0))
+print(type(1e3))
+
+print("complex:")
+print(type(1 + 2j))
+
+print("str:")
+print(type("Hola!"))
+
+print("bool:")
+print(type(True))
+print(type(False))
+print(type(1 < 2))
+
+print("NoneType:")
+print(type(None))
