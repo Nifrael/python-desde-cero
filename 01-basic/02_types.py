a0540856326b98738fc9d290e7364284160006ef
Julien Marchand <nifrael66@gmail.com>
2025-01-15 18:56:48 +0100

    2e partie

---
+++ 01-basic/02_types.py
@@ -0,0 +1,11 @@
+###
+# 01 - types()
+# Python a plusieurs types de données
+# integer, float, complex, string, booléens, NoneType...
+###
+
+print("int:")
+# print(type(10))
+# print(type(0))
+# print(type(-5))
+# print(type(34787593875923759237590723))
